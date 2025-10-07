2fec252a620818636b58d48077fbf1d6ad28191d
Gene Boggs <gene@ology.net>
2025-10-07 16:12:24 -0700

    WIP

--- examples/figured-bass.py
+++ examples/figured-bass.py
@@ -104,8 +104,7 @@ if __name__ == "__main__":
     br = Rhythm(
         measure_size=3,
         durations=[1/2, 1, 3/2],
-    )
-    
+    )    
     line_a = br.motif()
     chords_a = ['C','Em','F','G','Am']
     chord_a = random.choice(chords_a)
